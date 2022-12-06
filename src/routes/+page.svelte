70b27f79a1b2f3465ba00609c94ffb5fc6913fd9
Larry Masullo <lmasullo@gmail.com>
2022-12-06 09:32:03 -0600

    working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -86,15 +86,21 @@
         medfactsCatId: ${selectedNew}, 
         topic: "${newTopic}",
         fact: """${newFact}""",
+				favorite: false,
         userEditId: 1
       }) {
         id
         topic
+				fact
+				favorite
       }
     }`;
 		const respNewFact = await newGraphQLClient.request(createNewFact);
 		console.log(respNewFact);
 		message = 'Fact Added!';
+
+		// Add the new fact to the facts array
+		facts.push(respNewFact.medfactsFactCreate);
 
 		showAddFact = false;
 
@@ -199,7 +205,8 @@
       }) {
         id,
         topic,
-        fact      
+        fact,
+				favorite
       }
     }`;
 
@@ -272,119 +279,117 @@
 </script>
 
 <main>
-	<h1 class="center">Medfacts</h1>
-	<div id="divLoggedIn">
-		<div class="center">
-			<button id="btnDisplayAdd" class="btn btnPrimary" on:click={fnDisplayAdd}>
-				Add New Fact
-			</button>
-			<br />
-			{#if message !== ''}
-				<div class="message">
-					{message}
-				</div>
-			{/if}
+	<div class="center">
+		<h1>Medfacts</h1>
+		<button id="btnDisplayAdd" class="btn btnPrimary" on:click={fnDisplayAdd}>
+			Add New Fact
+		</button>
+		<br />
+		{#if message !== ''}
+			<div class="message">
+				{message}
+			</div>
+		{/if}
 
-			{#if showAddFact}
-				<div id="divAddFact">
-					<select id="selectCatNew" bind:value={selectedNew}>
-						<option value="">Choose a Category New</option>
-						{#each cats as cat}
-							<option value={cat.medfactsCatID}>{cat.cat}</option>
-						{/each}
-					</select>
-					<br />
-					<label for="textTopic">Topic</label>
-					<br />
-					<input id="textTopic" class="inputEl" type="text" bind:value={newTopic} />
-					<br />
-					<label for="textAreaFact">Fact</label>
-					<textarea id="textAreaFact" bind:value={newFact} cols="30" rows="10" />
-					<button id="btnSave" class="btn btnSuccess" on:click={fnSaveNew}> Save New Fact </button>
-					<button id="btnCancel" class="btn btnWarning" on:click={fnCancel}> Cancel </button>
-					<hr />
-				</div>
-			{/if}
-
-			<!-- Reactive -->
-			{#if !showAddFact}
-				<select id="selectCat" bind:value={selected} on:change={fnSelect}>
-					<option value="">Choose a Category</option>
-					{#each data.allCategories as cat}
+		{#if showAddFact}
+			<div id="divAddFact">
+				<select id="selectCatNew" bind:value={selectedNew}>
+					<option value="">Choose a Category New</option>
+					{#each cats as cat}
 						<option value={cat.medfactsCatID}>{cat.cat}</option>
 					{/each}
 				</select>
-
 				<br />
-				or Type to Search:
+				<label for="textTopic">Topic</label>
 				<br />
-
-				<input type="text" class="inputEl" bind:value={checkString} />
+				<input id="textTopic" class="inputEl" type="text" bind:value={newTopic} />
 				<br />
-				<button class="btn btnSecondary" on:click={showFavs}>Just Favorites</button>
-				<button class="btn btnWarning" on:click={reset}>Clear Filters</button>
+				<label for="textAreaFact">Fact</label>
+				<textarea id="textAreaFact" bind:value={newFact} cols="30" rows="10" />
+				<button id="btnSave" class="btn btnSuccess" on:click={fnSaveNew}> Save New Fact </button>
+				<button id="btnCancel" class="btn btnWarning" on:click={fnCancel}> Cancel </button>
+				<hr />
+			</div>
+		{/if}
 
-				<div>Number of Facts: {reactiveFacts.length}</div>
-				<br />
-				<div>
-					<strong>Click Topic to Edit Fact</strong>
-				</div>
+		<!-- Reactive -->
+		{#if !showAddFact}
+			<select id="selectCat" bind:value={selected} on:change={fnSelect}>
+				<option value="">Choose a Category</option>
+				{#each data.allCategories as cat}
+					<option value={cat.medfactsCatID}>{cat.cat}</option>
+				{/each}
+			</select>
 
-				<!-- {#if isLoading}
+			<br />
+			or Type to Search:
+			<br />
+
+			<input type="text" class="inputEl" bind:value={checkString} />
+			<br />
+			<button class="btn btnSecondary" on:click={showFavs}>Just Favorites</button>
+			<button class="btn btnWarning" on:click={reset}>Clear Filters</button>
+
+			<div>Number of Facts: {reactiveFacts.length}</div>
+			<br />
+			<div>
+				<strong>Click Topic to Edit Fact</strong>
+			</div>
+
+			<!-- {#if isLoading}
 					<div id="loading" class="bold">Loading...</div>
 				{/if} -->
 
-				<!-- {#if facts.length === 0 && isLoading === false}
+			<!-- {#if facts.length === 0 && isLoading === false}
 					<div id="loading" class="bold">No Matching Facts</div>
 				{/if} -->
-			{/if}
-			<!-- </div> -->
-			{#if !showAddFact}
-				{#each reactiveFacts as fact}
-					<div class="card">
-						<div class="cardHeading">
-							{#if edit === fact.id}
-								<input id="topic-{fact.id}" type="text" class="inputEl" value={fact.topic} />
-							{:else}
-								<div id="cardTopic" on:click={() => fnEdit(fact.id)}>
-									{fact.topic}
-								</div>
-							{/if}
-							<div id="favorite">
-								{#if fact.favorite === true}
-									<img
-										id="imgFav-{fact.id}"
-										src="fav.png"
-										alt="favorite"
-										on:click={() => fnFav(fact.id, fact.favorite)}
-									/>
-								{:else}
-									<img
-										id="imgFav-{fact.id}"
-										src="favblank.png"
-										alt="favorite"
-										on:click={() => fnFav(fact.id, fact.favorite)}
-									/>
-								{/if}
-							</div>
-						</div>
+		{/if}
+		<!-- </div> -->
+		{#if !showAddFact}
+			{#each reactiveFacts as fact}
+				<div class="card">
+					<div class="cardHeading">
 						{#if edit === fact.id}
-							<div class="edit">
-								<textarea id="fact-{fact.id}" value={fact.fact} />
-								<br />
-								<button class="btn btnSuccess" on:click={() => fnSave(fact.id)}>
-									Save Changes
-								</button>
-								<button class="btn btnDanger" on:click={() => fnDelete(fact.id)}> Delete </button>
-								<button class="btn btnWarning" on:click={fnCancelSave}>Cancel</button>
-							</div>
+							<input id="topic-{fact.id}" type="text" class="inputEl" value={fact.topic} />
 						{:else}
-							<div class="fact">{fact.fact}</div>
+							<div id="cardTopic" on:click={() => fnEdit(fact.id)}>
+								{fact.topic}
+							</div>
 						{/if}
+						<div id="favorite">
+							{#if fact.favorite === true}
+								<img
+									id="imgFav-{fact.id}"
+									src="fav.png"
+									alt="favorite"
+									on:click={() => fnFav(fact.id, fact.favorite)}
+								/>
+							{:else}
+								<img
+									id="imgFav-{fact.id}"
+									src="favblank.png"
+									alt="favorite"
+									on:click={() => fnFav(fact.id, fact.favorite)}
+								/>
+							{/if}
+						</div>
 					</div>
-				{/each}
-			{/if}
-		</div>
+					{#if edit === fact.id}
+						<div class="edit">
+							<textarea id="fact-{fact.id}" value={fact.fact} />
+							<br />
+							<button class="btn btnSuccess" on:click={() => fnSave(fact.id)}>
+								Save Changes
+							</button>
+							<button class="btn btnDanger" on:click={() => fnDelete(fact.id)}> Delete </button>
+							<button class="btn btnWarning" on:click={fnCancelSave}>Cancel</button>
+						</div>
+					{:else}
+						<div class="fact">{fact.fact}</div>
+					{/if}
+				</div>
+			{/each}
+		{/if}
 	</div>
 </main>
 
