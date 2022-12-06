fb68483f67d190e5496b300cf4e533662a745602
Larry Masullo <lmasullo@gmail.com>
2022-12-06 07:11:47 -0600

    copy edshifts

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { sveltekit } from '@sveltejs/kit/vite';
 import path from 'path';
 
@@ -6,10 +7,16 @@ const config = {
 	plugins: [sveltekit()],
 	resolve: {
 		alias: {
+			$comp: path.resolve('./src/lib/components'),
 			$css: path.resolve('./src/lib/css'),
-			$helper: path.resolve('./src/lib/helperFiles')
+			$helper: path.resolve('./src/lib/helperFiles'),
+			$api: path.resolve('./src/routes/api'),
+			$auth: path.resolve('./src/routes/auth')
 		}
 	}
+	// ssr: {
+	//     noExternal: ['devalue'],
+	// },
 };
 
 export default config;
