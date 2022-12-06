2881fb1f92b92146a1173709120b6487ff94c15b
Larry Masullo <lmasullo@gmail.com>
2022-12-06 09:54:36 -0600

    Using netlify env variables

--- src/lib/helperFiles/Env.js
+++ src/lib/helperFiles/Env.js
@@ -1,3 +1,7 @@
-export const ENDPOINT = import.meta.env.VITE_ENDPOINT;
-export const WORKSPACE_ID = import.meta.env.VITE_WORKSPACE_ID;
-export const TOKEN = import.meta.env.VITE_TOKEN;
+// export const ENDPOINT = import.meta.env.VITE_ENDPOINT;
+// export const WORKSPACE_ID = import.meta.env.VITE_WORKSPACE_ID;
+// export const TOKEN = import.meta.env.VITE_TOKEN;
+
+export const ENDPOINT = process.env.VITE_ENDPOINT;
+export const WORKSPACE_ID = process.env.VITE_WORKSPACE_ID;
+export const TOKEN = process.env.VITE_TOKEN;
